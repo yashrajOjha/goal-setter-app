d5bbc23ecf0456d4e3467eb4296602880b161a43
Yash <yashrajojah28@gmail.com>
2022-02-25 18:12:43 +0530

    UPDATE: Build routing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 5000 //current the port number is 8000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
 const connectDB = require('./config/db')
+const { sendFile } = require('express/lib/response')
 
 connectDB()
 
@@ -15,5 +17,15 @@ app.use(express.urlencoded({extended:false}))//url encoded parser
 app.use('/api/goals',require('./routes/goalRoutes')) //linking the routes
 /*get request to the goalRoutes using the api/goals*/
 app.use('/api/users',require('./routes/userRoutes'))
+
+//serve front for deployment
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build'))) //joining the two parts
+
+    //any other route will be rerouted to index.html
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+}else{
+    app.get('/',(req,res)=>res.send('Please set to production'))
+}
 app.use(errorHandler)
 app.listen(PORT,()=>console.log(`Server Started on Port ${PORT}`))
